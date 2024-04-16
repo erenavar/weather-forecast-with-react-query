098c443e3728dc88c540e70b7c1fdfb8cd5350f4
Eren AVAR <eren.avar@hotmail.com>
2024-04-16 16:25:53 +0200

    fetch-data-from-api

--- src/assets/api/WeatherApi.js
+++ src/assets/api/WeatherApi.js
@@ -1,6 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-
-
-export function getData(){
-    return 
+export function useData(city) {
+  return useQuery({
+    queryKey: ["forecasts"],
+    queryFn: () =>
+      axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5b7e1cbe85991511007fdc6496ee1595&units=metric`
+      ),
+  });
 }
