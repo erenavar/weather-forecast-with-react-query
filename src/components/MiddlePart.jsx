da9da50aa76bc14a3df0ceff2c14bb717b052f9f
Eren AVAR <eren.avar@hotmail.com>
2024-04-19 20:33:40 +0200

    add middle part elements without css

--- src/components/MiddlePart.jsx
+++ src/components/MiddlePart.jsx
@@ -1,7 +1,15 @@
 import "./MiddlePart.css";
 
 const MiddlePart = () => {
-  return <div className="middlePart"></div>;
+  return (
+    <div className="middlePart">
+      <div className="container">
+        <div>You can find out the weather by selecting the city</div>
+        <div>Select your city and teach the weather</div>
+        <button style={{ width: "10%" }}>Add</button>
+      </div>
+    </div>
+  );
 };
 
 export default MiddlePart;
