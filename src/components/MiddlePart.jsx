424db6379fe38652e29d482890fcafd1864a6764
Eren AVAR <eren.avar@hotmail.com>
2024-04-21 22:14:39 +0200

    styled middle part texts

--- src/components/MiddlePart.jsx
+++ src/components/MiddlePart.jsx
@@ -4,9 +4,16 @@ const MiddlePart = () => {
   return (
     <div className="middlePart">
       <div className="container">
-        <div>You can find out the weather by selecting the city</div>
-        <div>Select your city and teach the weather</div>
-        <button style={{ width: "10%" }}>Add</button>
+        <div style={{ lineHeight: "35px" }}>
+          <div className="fs-1 fw-bold">
+            You can find out the weather by selecting the city.
+          </div>
+          <div className="fs-3">Select your city and teach the weather.</div>
+        </div>
+
+        <br />
+
+        <button style={{ width: "10%" }}>+ Add</button>
       </div>
     </div>
   );
